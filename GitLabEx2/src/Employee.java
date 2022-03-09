ebd0b166ea690d1aca255086297aeba088909b92
admin <admin@Bcalab3-m03>
2022-03-09 10:46:15 +0530

    Commit-Employee prog

---
+++ GitLabEx2/src/Employee.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Employee {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+
+		System.out.println("Enter Employee Name");
+		Scanner sc = new Scanner(System.in);
+		
+		name= sc.nextLine();
+		
+		System.out.println("The Name you entered:" +name);
+
+	}
+
+}
--- Github2/src/Student.java
+++ Github2/src/Student.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 
 public class Student {
+	
+	String name;
+	
+	Student(String name){
+		String namee;
+		System.out.println("Please enter your name");
+		Scanner sc= new Scanner(System.in);
+		namee=sc.nextLine();
+		
+		name=namee;
+		System.out.println("The Name you entered:" +name);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("HELLO STUDENT");
+		Student s1= new Student();
 
 	}
 
